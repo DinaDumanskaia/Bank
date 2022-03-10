6a1aac3785f60b77d134e5f571affedd91019b2c
arakovskiy <arakovskiy@ocado.com>
2022-03-10 16:18:55 +0300

    changed client to a newer version

--- src/test/java/web/AcceptanceTest.java
+++ src/test/java/web/AcceptanceTest.java
@@ -3,49 +3,36 @@ package web;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLConnection;
-import java.util.UUID;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class AcceptanceTest {
-    UUID uuid;
-    URL url;
 
-    private BufferedReader getBufferedReader(String urlString) throws IOException {
-        url = new URL(urlString);
-        URLConnection conn = url.openConnection();
-        conn.setConnectTimeout(20);
-        return new BufferedReader(new InputStreamReader(conn.getInputStream()));
+    private int checkHead2(String urlInputString) throws IOException, InterruptedException, URISyntaxException {
+        return sendRequest(HeadRequest(urlInputString)).statusCode();
     }
 
-    private int checkHead(String urlInputString) throws IOException {
-        URL url = new URL(urlInputString);
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setRequestMethod("HEAD");
-        connection.setConnectTimeout(5000);
-        connection.setReadTimeout(5000);
-        int status = connection.getResponseCode();
-        return status;
+    private HttpResponse<String> sendRequest(HttpRequest request) throws IOException, InterruptedException {
+        return HttpClient.newBuilder()
+                .build()
+                .send(request, HttpResponse.BodyHandlers.ofString());
     }
 
-    @Test
-    public void testLocalHost() throws IOException {
-        Assert.assertEquals(500, checkHead("http://localhost:8080/bank/v1"));
-        Assert.assertEquals(404, checkHead("http://localhost:8080/bank/v1/clients/2406846c-3c01-4297-b8c2-8a960ddefce6"));
+    private HttpRequest HeadRequest(String urlInputString) throws URISyntaxException {
+        return HttpRequest.newBuilder()
+                .uri(new URI(urlInputString))
+                .method("HEAD", HttpRequest.BodyPublishers.noBody())
+                .build();
     }
 
     @Test
-    public void testPostNewClient() throws Exception {
-        Assert.assertEquals(200, checkHead("http://localhost:8080/bank/v1/clients/" + Controller.createClient()));
-    }
-
-    @Test
-    public void testNewClientsBalanceIsEmpty() throws Exception {
-        Assert.assertEquals(0, checkHead("http://localhost:8080/bank/v1/clients/" + Controller.createClient() + "/balance"));
+    public void testLocalHost() throws IOException, URISyntaxException, InterruptedException {
+        Assert.assertEquals(500, checkHead2("http://localhost:8080/bank/v1"));
+        Assert.assertEquals(404, checkHead2("http://localhost:8080/bank/v1/clients/2406846c-3c01-4297-b8c2-8a960ddefce6"));
     }
 
 }

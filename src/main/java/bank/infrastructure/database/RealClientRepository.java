e4ea597622d4fe76353892dead469192529ab20c
DinaDu <twodpho@gmail.com>
2022-03-17 18:43:54 +0300

    refactoring repository1

--- src/main/java/bank/infrastructure/database/RealClientRepository.java
+++ src/main/java/bank/infrastructure/database/RealClientRepository.java
@@ -20,35 +20,29 @@ public class RealClientRepository implements ClientRepository {
     @Override
     public boolean clientExists(UUID clientId) {
         try {
-            ResultSet resultSet = getStm().executeQuery("select *from CLIENTS where id = '" + clientId + "'");
-            if (resultSet.next()) return true;
+            ResultSet resultSet = getStatement().executeQuery("select *from CLIENTS where id = '" + clientId + "'");
+            return resultSet.next();
         } catch (SQLException ex) {
-            catchSQLException(ex);
+            throw new RepositoryError("Bad service connection");
         }
-        return false;
     }
 
-    private Statement getStm() throws SQLException {
-        var con = DriverManager.getConnection(DB_URL, "sa", "password");
-        var stm = con.createStatement();
-        return stm;
+    private Statement getStatement() throws SQLException {
+        return DriverManager.getConnection(DB_URL, "sa", "password").createStatement();
     }
 
     @Override
     public Client getClientById(UUID clientId) {
-
         try {
-            ResultSet resultSet = getStm().executeQuery("SELECT * FROM CLIENTS C\n" +
+            ResultSet resultSet = getStatement().executeQuery("SELECT * FROM CLIENTS C\n" +
                     "LEFT JOIN ACCOUNTS A ON A.CLIENT_ID = C.ID\n" +
                     "LEFT JOIN TRANSACTIONS TR ON TR.ACCOUNT_ID = A.ACCOUNT_ID \n" +
                     "WHERE C.ID = '" + clientId + "'");
 
             List<Transaction> transactions = new ArrayList<>();
             Currency currency = Currency.RUB;
-            UUID moneyAccountId = null;
-            if (!resultSet.next()) {
-                throw new ClientNotFoundException("Client not found");
-            }
+            UUID moneyAccountId;
+            throwIfNoClient(resultSet.next());
             do {
                 moneyAccountId = UUID.fromString(resultSet.getString("ACCOUNT_ID"));
                 if (resultSet.getTimestamp("TRANSACTION_DATE") == null) {
@@ -59,45 +53,48 @@ public class RealClientRepository implements ClientRepository {
                 UUID transactionId = UUID.fromString(resultSet.getString("TRANSACTION_ID"));
                 transactions.add(new Transaction(transactionId, transaction, transactionDate));
             } while (resultSet.next());
-
-
             return new Client(clientId, Map.of(currency, new MoneyAccount(moneyAccountId, transactions)));
-
         } catch (SQLException ex) {
-            catchSQLException(ex);
+            throw new RepositoryError("Bad service connection");
         }
-        throw new ClientNotFoundException("Client not found");
+    }
+    private void throwIfNoClient(boolean hasNext) throws SQLException {
+        if (!hasNext) {
+            throw new ClientNotFoundException("Client not found");
+        }
     }
 
     @Override
     public void saveClient(Client client) {
         try {
-            var con = DriverManager.getConnection(DB_URL, "sa", "password");
-            var stm = con.createStatement();
-            stm.execute("MERGE INTO CLIENTS VALUES ('" + client.getID() + "')");
+            var connection = DriverManager.getConnection(DB_URL, "sa", "password");
+            var statement = connection.createStatement();
             UUID account_id = client.getMoneyAccountId(Currency.RUB);
-            stm.execute("MERGE INTO ACCOUNTS VALUES('" + account_id + "', '" + client.getID() + "', 'RUB')");
-//            stm.execute("INSERT INTO TRANSACTIONS VALUES('" + UUID.randomUUID() + "', '" + account_id + "', '200', ?)");
-            for (Transaction transaction : client.getListOfTransactions()) {
-                Date date = transaction.getDate();
-                PreparedStatement preparedStatement = con.prepareStatement(
-                        "MERGE INTO TRANSACTIONS VALUES('" + transaction.getTransactionId() + "', '" + account_id + "', '" + transaction.getAmount() + "', ?)");
-                preparedStatement.setTimestamp(1, new Timestamp(date.getTime()));
-                preparedStatement.execute();
-            }
 
-//            if (rs.next()) {
-//
-//                System.out.println(rs.getInt(1));
-//            }
-
+            saveClient(client, statement);
+            saveAccount(client, statement, account_id);
+            saveTransactions(client, connection, account_id);
         } catch (SQLException ex) {
-            catchSQLException(ex);
+            throw new RepositoryError("Bad service connection");
         }
     }
 
-    private void catchSQLException(SQLException ex) {
-        throw new RepositoryError("Bad service connection");
+    private void saveAccount(Client client, Statement statement, UUID account_id) throws SQLException {
+        statement.execute("MERGE INTO ACCOUNTS VALUES('" + account_id + "', '" + client.getID() + "', 'RUB')");
+    }
+
+    private void saveClient(Client client, Statement statement) throws SQLException {
+        statement.execute("MERGE INTO CLIENTS VALUES ('" + client.getID() + "')");
+    }
+
+    private void saveTransactions(Client client, Connection connection, UUID account_id) throws SQLException {
+        for (Transaction transaction : client.getListOfTransactions()) {
+            Date date = transaction.getDate();
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                    "MERGE INTO TRANSACTIONS VALUES('" + transaction.getTransactionId() + "', '" + account_id + "', '" + transaction.getAmount() + "', ?)");
+            preparedStatement.setTimestamp(1, new Timestamp(date.getTime()));
+            preparedStatement.execute();
+        }
     }
 
 }

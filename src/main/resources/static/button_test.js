379e27f4a34d1bc2c7229188543af3f336e695e0
DinaDu <twodpho@gmail.com>
2022-03-24 17:33:44 +0300

    транзакции выводятся на экран

--- src/main/resources/static/button_test.js
+++ src/main/resources/static/button_test.js
@@ -1,18 +1,26 @@
 
 const e = React.createElement;
 
+
+
+
 class ClientPage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
         clientId : props.clientId,
-        balance : props.balance
+        balance : props.balance,
+        foo: null,
+        value3 : '',
+        transactions: []
     };
 
     this.handleChangeBalance = this.handleChangeBalance.bind(this);
     this.handleSubmitBalanceModify = this.handleSubmitBalanceModify.bind(this);
 
+this.handleSubmitGetTransaction = this.handleSubmitGetTransaction.bind(this);
   }
+
 
     componentDidUpdate() {
         fetch('http://localhost:8080/bank/v1/clients/' + this.props.clientId)
@@ -31,9 +39,25 @@ class ClientPage extends React.Component {
         event.preventDefault();
     }
 
-  handleChangeBalance(event) {
-    this.setState({value1: event.target.value});
-  }
+    handleChangeBalance(event) {
+        this.setState({value1: event.target.value});
+    }
+
+
+
+      handleSubmitGetTransaction(event) {
+          this.setState({foo: true});
+          fetch('http://localhost:8080/bank/v1/clients/' + this.props.clientId + '/transactions/')
+                .then(res => res.json())
+                .then(res => res.map(transaction => transaction.amount))
+                .then(transactionAmounts => this.setState({transactions : transactionAmounts}));
+   }
+
+       handleChangeId(event) {
+          this.setState({value3: event.target.value});
+        }
+
+
 
   ClientInfo() {
     return (
@@ -51,12 +75,50 @@ class ClientPage extends React.Component {
                     <input type="submit" value="Перевести" />
                 </form>
             <br />
+                <form onSubmit={this.handleSubmitGetTransaction}>
+                <label>
+                    Запросить foo:
+                </label>
+                    <input type="submit" value="FOO" />
+                </form>
         </div>
         );
     }
 
 
+    Transactions() {
+        return (
+            <div>
+                <br />
+                    <form onSubmit={this.handleSubmitGetTransaction}>
+                    <label>
+                        Запросить foo:
+                    </label>
+                        <input type="submit" value="FOO" />
+                    </form>
+            </div>
+            );
+        }
+//
+//              <ul>
+//                {this.state.transactions.map(item => {
+//                  return <li>{item}</li>;
+//                })}
+//              </ul>
 render() {
+
+
+    if (this.state.foo) {
+        return (
+            <ul>
+                {this.state.transactions.map(item => {
+                return <li>{item}</li>;
+                })}
+            </ul>
+
+            );
+    }
+
     return (
     <div>
         {this.ClientInfo()}
@@ -72,11 +134,10 @@ class MainPage extends React.Component {
         value: '',
         balance: null,
         clientId: null,
-        kek: null,
+        buttonCreate: null,
     };
 
     this.handleSubmitCreate = this.handleSubmitCreate.bind(this);
-
 
     this.handleSubmitGetClient = this.handleSubmitGetClient.bind(this);
     this.handleChangeId = this.handleChangeId.bind(this);
@@ -87,7 +148,7 @@ class MainPage extends React.Component {
  }
 
   handleSubmitCreate(event) {
-    this.setState({kek: true});
+    this.setState({buttonCreate: true});
     const requestOptions = {
         method: 'POST',
     };
@@ -100,12 +161,12 @@ class MainPage extends React.Component {
 
 
   handleSubmitGetClient(event) {
-      this.setState({kek: true});
+      this.setState({buttonCreate: true});
       fetch('http://localhost:8080/bank/v1/clients/' + this.state.value)
             .then(response => response.json())
             .then(data => this.setState({ clientId: data.id, balance: data.balance }));
       event.preventDefault();
-    }
+   }
 
   main() {
     return (
@@ -119,7 +180,7 @@ class MainPage extends React.Component {
            <br />
              <form onSubmit={this.handleSubmitGetClient}>
                <label>
-                 Введите идентификатор клиента:
+                 ИД:
                    <input type="text" value={this.state.value} onChange={this.handleChangeId} />
                </label>
                <input type="submit" value="Найти клиента" />
@@ -130,7 +191,7 @@ class MainPage extends React.Component {
 
 
   render() {
-    if (this.state.kek) {
+    if (this.state.buttonCreate) {
         return (
             <div>
                 <ClientPage clientId={this.state.clientId} balance={this.state.balance} />
@@ -146,8 +207,6 @@ class MainPage extends React.Component {
 
   }
 }
-
-
 
 
 ReactDOM.render(
